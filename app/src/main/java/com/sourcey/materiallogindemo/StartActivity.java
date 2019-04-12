4d1bcfb0ce3394a1b3324109b70dda4a1a3fa25e
david RN <davidrabano@metricool.com>
2019-04-12 18:17:53 +0200

    Añadido nuevo menu desplegable

--- app/src/main/java/com/sourcey/materiallogindemo/StartActivity.java
+++ app/src/main/java/com/sourcey/materiallogindemo/StartActivity.java
@@ -1,21 +1,99 @@
 package com.sourcey.materiallogindemo;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
 
 public class StartActivity extends AppCompatActivity {
+    String[] listArray;
+    ListView drawerListView;
+    ActionBarDrawerToggle mActionBarDrawerToggle;
+    DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        listArray = getResources().getStringArray(R.array.listArray);
+        drawerListView = (ListView)findViewById(R.id.left_drawer);
+        drawerListView.setAdapter(new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item, listArray));
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        //Create Drawer Toggle
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open_drawer, R.string.close_drawer){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        mDrawerLayout.addDrawerListener(mActionBarDrawerToggle);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
     }
 
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        //Share Action Provider
+        MenuItem menuItem = menu.findItem(R.id.action_logo);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        if(mActionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.action_logo:
+                Intent intent = new Intent(this, StartActivity.class);
+                startActivity(intent);
+                break;
+            /*case R.id.item_list:
+                Intent intent2 = new Intent(this, LoginActivity.class);
+                startActivity(intent2);
+                break;*/
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mActionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig){
+        super.onConfigurationChanged(newConfig);
+        mActionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+   /* public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu items for use in the action bar
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
@@ -34,5 +112,5 @@ public class StartActivity extends AppCompatActivity {
         }
 
         return true;
-    }
+    }*/
 }
