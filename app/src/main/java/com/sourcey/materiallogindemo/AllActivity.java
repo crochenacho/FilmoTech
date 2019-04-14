fd8b25fa34fafa7ae29377c14d287d00d3700622
Alex Vizca <alejandro.vizcaino.alemany@alumnos.upm.es>
2019-04-14 19:52:31 +0200

    añadir funcionalidad drawer y layouts faltantes

--- app/src/main/java/com/sourcey/materiallogindemo/AllActivity.java
+++ app/src/main/java/com/sourcey/materiallogindemo/AllActivity.java
@@ -1,22 +1,142 @@
 package com.sourcey.materiallogindemo;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
 
 public class AllActivity extends AppCompatActivity {
+    String[] listArray;
+    ListView drawerListView;
+    ActionBarDrawerToggle mActionBarDrawerToggle;
+    DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all);
+        listArray = getResources().getStringArray(R.array.listArray);
+        drawerListView = (ListView)findViewById(R.id.left_drawer);
+        drawerListView.setAdapter(new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item, listArray));
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        //Create Drawer Toggle
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open_drawer, R.string.close_drawer){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        mDrawerLayout.addDrawerListener(mActionBarDrawerToggle);
+        drawerListView.setOnItemClickListener(new AllActivity.DrawerItemClickListener());
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
     }
 
+    private void selectItem(int position) {
+        Fragment fragment = null;
+        Intent intent = null;
+        switch(position){
+            case 0:
+                startActivity(new Intent(this, AllActivity.class));
+                return;
+            case 1:
+                intent = new Intent(this, AllActivity.class);
+                startActivity(intent);
+                return;
+            case 2:
+                intent = new Intent(this, AllActivity.class);
+                startActivity(intent);
+                return;
+            case 3:
+                intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return;
+            case 4:
+                intent = new Intent(this, AboutUsActivity.class);
+                startActivity(intent);
+                return;
+            case 5:
+                logout();
+                return;
+            default:
+                break;
+        }
+
+    }
+
+    private void logout() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu items for use in the action bar
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        //Share Action Provider
+        MenuItem menuItem = menu.findItem(R.id.action_logo);
+
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        if(mActionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.action_logo:
+                Intent intent = new Intent(this, StartActivity.class);
+                startActivity(intent);
+                break;
+            /*case R.id.item_list:
+                Intent intent2 = new Intent(this, LoginActivity.class);
+                startActivity(intent2);
+                break;*/
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mActionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig){
+        super.onConfigurationChanged(newConfig);
+        mActionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    private class DrawerItemClickListener implements AdapterView.OnItemClickListener {
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
+        }
     }
 }
